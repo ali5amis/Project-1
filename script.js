6c948670bdf1141602dd497b2d96ee9d33b1c680
Ali Khamis <3lijaffer2000@gmail.com>
2024-05-06 09:49:18 +0300

    global varible

---
+++ script.js
@@ -0,0 +1,7 @@
+const cards = document.querySelectorAll('.card')
+const front = document.querySelectorAll('.front')
+const exit = document.querySelector('.exit')
+const score = document.querySelector('.score')
+const txtscore = document.querySelector('.textscore')
+const restartgame = document.querySelector('.playagain')
+let scores = 0
