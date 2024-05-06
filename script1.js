6d0e2860407229f6df92dd678aeaaec154f1e3b5
Ali Khamis <3lijaffer2000@gmail.com>
2024-05-06 10:13:03 +0300

    application function

--- script1.js
+++ script1.js
@@ -6,51 +6,53 @@ const txtscore = document.querySelector('.textscore')
 const restartgame = document.querySelector('.playagain')
 let scores = 0
 
-
 function clicksquare() {
-    cards.forEach((card) => {
-      const everysquare = [...Array(cards.length).keys()]
-      const cardsrandom = Math.floor(Math.random() * cards.length)
-  
-      card.style.order = everysquare[cardsrandom]
+  cards.forEach((card) => {
+    const everysquare = [...Array(cards.length).keys()]
+    const cardsrandom = Math.floor(Math.random() * cards.length)
+
+    card.style.order = everysquare[cardsrandom]
+  })
+}
+
+function clicktoshow() {
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('click', () => {
+      front[i].classList.add('flip')
+      const frontid = parseInt(front[i].getAttribute('id'))
+      if (frontid >= 5) {
+        scores++
+        score.textContent = scores
+        if (scores >= 8) {
+          txtscore.textContent = 'You are Won this game ^^'
+          flipAll()
+        }
+      } else {
+        scores = 0
+        txtscore.textContent = 'You are loser'
+        flipAll()
+      }
     })
   }
-  
-  function clicktoshow() {
-    for (let i = 0; i < cards.length; i++) {
-      cards[i].addEventListener('click', () => {
-        front[i].classList.add('flip')
-        const frontid = parseInt(front[i].getAttribute('id'))
-        if (frontid >= 5) {
-            scores++
-            score.textContent = scores
-            if (scores >= 8) {
-                txtscore.textContent = 'You are Won this game ^^'
-                flipAll()
-              }
-          } else {
-            scores = 0
-            txtscore.textContent = 'You are loser'
-            flipAll()
-          }    
-      })
-    }
-  }
-  
-  function flipAll() {
-    front.forEach((c) => {
-      console.log(`c = ${c}`)
-      c.classList.add('flip')
-    })
-  }
+}
 
-  function quitgame() {
-    window.close()
-  }
-  
-  function playagain() {
-    location.reload()
-  }
+function flipAll() {
+  front.forEach((c) => {
+    console.log(`c = ${c}`)
+    c.classList.add('flip')
+  })
+}
 
-  exit.addEventListener('click', quitgame)
-  restartgame.addEventListener('click', playagain)
+function quitgame() {
+  window.close()
+}
+
+function playagain() {
+  location.reload()
+}
+
+exit.addEventListener('click', quitgame)
+restartgame.addEventListener('click', playagain)
+
+clicksquare()
+clicktoshow()
