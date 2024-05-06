c9a58d5eae80e2ce9b71a2fe157e936db1ef9802
Ali Khamis <3lijaffer2000@gmail.com>
2024-05-06 09:50:22 +0300

    function to clicksquare

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ const score = document.querySelector('.score')
 const txtscore = document.querySelector('.textscore')
 const restartgame = document.querySelector('.playagain')
 let scores = 0
+
+function clicksquare() {
+    cards.forEach((card) => {
+      const everysquare = [...Array(cards.length).keys()]
+      const cardsrandom = Math.floor(Math.random() * cards.length)
+  
+      card.style.order = everysquare[cardsrandom]
+    })
+  }
