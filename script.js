1260bc3056a0b207a52fd142a422fa51bd76319e
Ali Khamis <3lijaffer2000@gmail.com>
2024-05-06 09:55:28 +0300

    flip all

--- script.js
+++ script.js
@@ -7,28 +7,31 @@ const restartgame = document.querySelector('.playagain')
 let scores = 0
 
 function clicksquare() {
-    cards.forEach((card) => {
-      const everysquare = [...Array(cards.length).keys()]
-      const cardsrandom = Math.floor(Math.random() * cards.length)
-  
-      card.style.order = everysquare[cardsrandom]
+  cards.forEach((card) => {
+    const everysquare = [...Array(cards.length).keys()]
+    const cardsrandom = Math.floor(Math.random() * cards.length)
+
+    card.style.order = everysquare[cardsrandom]
+  })
+}
+
+function clicktoshow() {
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('click', () => {
+      front[i].classList.add('flip')
+      const frontid = parseInt(front[i].getAttribute('id'))
+      if (frontid >= 3 && frontid <= 12) {
+        scores++
+        score.textContent = scores
+      } else if (frontid <= 2) {
+        scores = 0
+        txtscore.textContent = 'You are loser'
+      }
     })
   }
-
-  function clicktoshow() {
-    for (let i = 0; i < cards.length; i++) {
-      cards[i].addEventListener('click', () => {
-        front[i].classList.add('flip')
-        const frontid = parseInt(front[i].getAttribute('id'))
-        if (frontid >= 3 && frontid <= 12) {
-            scores++
-            score.textContent = scores
-          } else if (frontid <= 2) {
-            scores = 0
-            txtscore.textContent = 'You are loser'
-          }
-  
-      })
-    }
-  }
-  
+}
+const flipAll = () => {
+  front.forEach((c) => {
+    c.classList.add('flip')
+  })
+}
