0a4930895a1a587f66d4108737b10a54b45ee222
Ali Khamis <3lijaffer2000@gmail.com>
2024-05-07 12:54:38 +0300

    font family & darkmode

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ const restartgame = document.querySelector('.playagain')
 const modal = document.getElementById('myModal')
 const btntips = document.getElementById('tips')
 var span = document.getElementsByClassName('close')[0]
+const dm = document.querySelector('.darkmode')
 let scores = 0
+let dmwhite = 1
 
+//to random cards
 function clicksquare() {
   cards.forEach((card) => {
     const everysquare = [...Array(cards.length).keys()]
@@ -19,27 +22,32 @@ function clicksquare() {
 }
 
 function clicktoshow() {
+  // to select all card
   for (let i = 0; i < cards.length; i++) {
     cards[i].addEventListener('click', () => {
       front[i].classList.add('flip')
       const frontid = parseInt(front[i].getAttribute('id'))
+      //if collect fruit will + 1 score
       if (frontid >= 3 && frontid <= 12) {
         scores++
         score.textContent = scores
+        //if collect fruit
         if (scores >= 5) {
-          txtscore.style.background='green'
+          txtscore.style.background = 'green'
           txtscore.textContent = 'You are Won, You can next Level 2'
           flipAll()
         }
+        //if collect bomb
       } else if (frontid <= 2) {
         scores = 0
-        txtscore.style.background='red'
+        txtscore.style.background = 'red'
         txtscore.textContent = 'You are loser'
         flipAll()
       }
     })
   }
 }
+//to flip all front
 const flipAll = () => {
   front.forEach((c) => {
     c.classList.add('flip')
@@ -67,6 +75,19 @@ window.onclick = function (event) {
   }
 }
 
+const dmblack = () => {
+  if (dmwhite === 1) {
+    document.body.style.backgroundColor = 'Black'
+    dmwhite--
+  } else {
+    document.body.style.backgroundColor = 'rgb(51, 89, 122)'
+    dmwhite++
+  }
+}
+
+dm.addEventListener('click', () => {
+  dmblack()
+})
 exit.addEventListener('click', quitgame)
 restartgame.addEventListener('click', playagain)
 
--- script1.js
+++ script1.js
@@ -7,7 +7,9 @@ const restartgame = document.querySelector('.playagain')
 const modal = document.getElementById('myModal')
 const btntips = document.getElementById('tips')
 var span = document.getElementsByClassName('close')[0]
+const dm = document.querySelector('.darkmode')
 let scores = 0
+let dmwhite = 1
 
 function clicksquare() {
   cards.forEach((card) => {
@@ -25,16 +27,15 @@ function clicktoshow() {
       const frontid = parseInt(front[i].getAttribute('id'))
       if (frontid >= 5) {
         scores++
-        score.style.color = 'red'
         score.textContent = scores
         if (scores >= 8) {
-          txtscore.style.color = 'lightGreen'
+          txtscore.style.background = 'green'
           txtscore.textContent = 'You are Won this game ^^'
           flipAll()
         }
       } else {
         scores = 0
-        txtscore.style.color = 'red'
+        txtscore.style.background = 'red'
         txtscore.textContent = 'You are loser'
         flipAll()
       }
@@ -69,6 +70,20 @@ window.onclick = function (event) {
     modal.style.display = 'none'
   }
 }
+
+const dmblack = () => {
+  if (dmwhite === 1) {
+    document.body.style.backgroundColor = 'Black'
+    dmwhite--
+  } else {
+    document.body.style.backgroundColor = 'rgb(51, 89, 122)'
+    dmwhite++
+  }
+}
+
+dm.addEventListener('click', () => {
+  dmblack()
+})
 
 exit.addEventListener('click', quitgame)
 restartgame.addEventListener('click', playagain)
